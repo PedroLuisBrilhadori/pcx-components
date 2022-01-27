9a012fa6d33bcfcdf54df336bb826cd01d3b5b05
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-01-27 00:38:08 -0300

    feat: cria arquivo html

--- src/app/examples/examples.component.ts
+++ src/app/examples/examples.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'pcx-examples',
-  template: ` <pcx-tool-bar title="PcxComponents"></pcx-tool-bar> `,
+  templateUrl: 'examples.component.html',
   styleUrls: ['./examples.component.scss'],
 })
 export class PcxExamplesComponent implements OnInit {
